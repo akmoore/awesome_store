08c43d38ed9d86400dc7f953c00bd674c9c4d765
Alfred Moore <ak_moore@live.com>
2017-11-22 10:57:55 -0600

    Add validations and Admin Level to User

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ApplicationRecord
   # has_attached_file :avatar, styles: {medium: '300x300>', thumb: '100x100>'} #=> keep deminsions
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_presence_of :name, :price, :quantity, :brand, :category
+  validates :name, length: {minimum: 3}
+  validates_numericality_of :price, greater_than_or_equal_to: 0.01
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
   
 end
 
