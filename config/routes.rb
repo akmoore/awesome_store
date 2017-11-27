4def0d99006bfb6dda52ef2c25f0b0ed830e9b2a
Alfred Moore <ak_moore@live.com>
2017-11-27 09:31:00 -0600

    Added Storefront controller, setup Storefront index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'shop' => 'storefront#index'
+
+  get 'about' => 'storefront#about'
+
   devise_for :users
   scope :admin do
     resources :products
     resources :brands
     resources :categories
   end
-  root 'products#index'
+
+  root 'storefront#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
