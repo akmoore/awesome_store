b5389cdc196b4769c2f55b4080dc31493abeade1
Alfred Moore <ak_moore@live.com>
2017-11-28 10:54:34 -0600

    Setup Shopping Cart"

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,15 @@ class CartsController < ApplicationController
   
   def edit
   end
+
+  def destroy
+    if @cart.destroy
+      session[:cart_id] = nil
+      flash[:notice] = "Your cart was successfully deleted."
+      redirect_to shop_path
+    else
+      flash[:notice] = "Couldn't delete cart"
+      redirect_to edit_cart_path #=> could also be edit_cart_url
+    end
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,16 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy
+
+    def subtotal
+        #****** RUBY WAY ******
+        # line_items.to_a.sum do |item|
+        #     item.total
+        # end
+        # line_items.to_a.sum {|item| item.total} #=> short form version of the previous method
+
+        #***** SQL WAY, MUCH FASTER *****
+        line_items.select("SUM(quantity * price) AS sum")[0].sum
+    end
 end
 
 # == Schema Information
