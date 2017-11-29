00759a9d466fcb52fdf9643596210dbbc96ad563
Alfred Moore <ak_moore@live.com>
2017-11-29 11:01:37 -0600

    Added create method in order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,9 +15,26 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.add_line_items_from_cart(@cart)
+
+    if @order.save
+        Cart.destroy(session[:cart_id]) #=> destroy the cart that the browser is using, so we're pulling it out of the session
+        session[:cart_id] = nil
+        redirect_to shop_url, notice: 'Thanks for your order.'
+    else
+      render :new #=> this will run the new method within the controller, render a new form to save the order.
+    end
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :address, :pay_type, :user_id)
   end
 
 end
