b543aae2cb9090020fa5e8511681ae414676cb74
Alfred Moore <ak_moore@live.com>
2017-12-05 10:50:56 -0600

    Updated pagination in the storefront controller in index action/method

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,10 +1,10 @@
 class StorefrontController < ApplicationController
   def index
     if params[:search]
-      @products = Product.search_by_name_or_description(params[:search], params[:page])
+      @products = Product.search_by_name_or_description(params[:search], params[:page]).paginate(:page => params[:page])
       if @products.empty?
         flash.now[:notice] = "No results found for #{params[:search]}, showing all products instead."
-        @products = Product.all
+        @products = Product.all.paginate(:page => params[:page])
       end
     elsif params[:category_id]
       @category = Category.find(params[:category_id])
